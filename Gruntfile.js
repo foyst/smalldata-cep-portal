bb7a598d6224ecd4d32ed8339fd62b4c022bc8e2
foyst <benfoster88@hotmail.com>
2016-08-25 10:13:39 +0100

    Added keepClosingSlash option to htmlmin to avoid minifying issues with self closing tags

--- Gruntfile.js
+++ Gruntfile.js
@@ -305,7 +305,8 @@ module.exports = function (grunt) {
           collapseWhitespace: true,
           conservativeCollapse: true,
           collapseBooleanAttributes: true,
-          removeCommentsFromCDATA: true
+          removeCommentsFromCDATA: true,
+          keepClosingSlash: true
         },
         files: [{
           expand: true,
